8992a810d56b0b49479fd93275096154c0c1e445
Alfredo Pedraza <alfpedraza@hotmail.com>
2020-11-07 03:03:04 +0000

    Add BinarySearch code.

--- src/main/java/com/pedraza/algorithms/BinarySearch.java
+++ src/main/java/com/pedraza/algorithms/BinarySearch.java
@@ -1,5 +1,27 @@
 package com.pedraza.algorithms;
 
-public class BinarySearch {
+public class BinarySearch<T extends Comparable<T>> {
+
+    private T[] data;
+
+    public BinarySearch(T[] data) {
+        this.data = data;
+    }
+
+    public T[] data() { return data; }
     
+    public int binarySearch(T value) {
+        return binarySearch(0, data.length - 1, value);
+    }
+
+    private int binarySearch(int first, int last, T value) {
+        if (last < first) return -1;
+        int mid = (first + last) / 2;
+        if (data[mid] == value) return mid;
+        if (data[mid].compareTo(value) > 0) {
+            return binarySearch(first, mid -1, value);
+        } else {
+            return binarySearch(mid + 1, last, value);
+        }
+    }
 }
---
+++ src/test/java/com/pedraza/algorithms/BinarySearchTest.java
@@ -0,0 +1,57 @@
+package com.pedraza.algorithms;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+    BinarySearch<Integer> search;
+
+    @Test
+    public void searchShouldReturnIndexWhenFound() {
+        Integer[] data = new Integer[] { 9, 18, 23, 32, 50, 61 };
+        assertBinarySearch(data, 61, 5);
+    }
+
+    @Test
+    public void searchShouldReturnMinusOneWhenNotFound() {
+        Integer[] data = new Integer[] { 9, 18, 23, 32, 50, 61 };
+        assertBinarySearch(data, 33, -1);
+    }
+
+    @Test
+    public void searchShouldReturnMinusOneWhenNotFoundOneItem() {
+        Integer[] data = new Integer[] { 9 };
+        assertBinarySearch(data, 2, -1);
+    }
+
+    @Test
+    public void searchShouldReturnMinusOneWhenNotFoundEmptyItem() {
+        Integer[] data = new Integer[] { };
+        assertBinarySearch(data, 2, -1);
+    }
+
+    @Test
+    public void searchShouldReturnWhenItemAtLeft() {
+        Integer[] data = new Integer[] { 1, 2, 3, 4, 5 };
+        assertBinarySearch(data, 2, 1);
+    }
+
+    @Test
+    public void searchShouldReturnWhenItemAtRight() {
+        Integer[] data = new Integer[] { 1, 2, 3, 4, 5 };
+        assertBinarySearch(data, 4, 3);
+    }
+
+    @Test
+    public void searchShouldReturnWhenItemAtMid() {
+        Integer[] data = new Integer[] { 1, 2, 3, 4, 5 };
+        assertBinarySearch(data, 3, 2);
+    }
+
+    private void assertBinarySearch(Integer[] data, int value, int expected) {
+        search = new BinarySearch<Integer>(data);
+        int actual = search.binarySearch(value);
+        assertEquals(expected, actual);
+    }
+}
