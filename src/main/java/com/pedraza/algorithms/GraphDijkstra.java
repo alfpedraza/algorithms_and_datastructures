a9b68222d8c97bc6633927af2198409a91379a61
Alfredo Pedraza <alfpedraza@hotmail.com>
2020-11-08 05:52:00 +0000

    Complete Dijkstra algorithm

--- src/main/java/com/pedraza/algorithms/GraphDijkstra.java
+++ src/main/java/com/pedraza/algorithms/GraphDijkstra.java
@@ -1,5 +1,64 @@
 package com.pedraza.algorithms;
 
-public class GraphDijkstra {
+import com.pedraza.datastructures.ArrayList;
+import com.pedraza.datastructures.Graph;
+import com.pedraza.datastructures.GraphEdge;
+import com.pedraza.datastructures.GraphVertex;
+import com.pedraza.datastructures.HashTable;
+import com.pedraza.datastructures.Stack;
+
+public class GraphDijkstra<T> {
     
+    private Graph<T, Integer> graph;
+
+    public GraphDijkstra(Graph<T, Integer> graph) {
+        this.graph = graph;
+    }
+
+    public ArrayList<T> search(T start, T end) {
+
+        // Initialization
+        GraphVertex<T, Integer> startVertex = graph.get(start);
+        if (startVertex == null) return null;
+
+        HashTable<T, Integer> distance = new HashTable<>();
+        HashTable<T,T> previous = new HashTable<>();
+        HashTable<T, Boolean> visited = new HashTable<>();
+        Stack<GraphVertex<T, Integer>> stack = new Stack<>();
+
+        distance.set(start, 0);
+        previous.set(start, null);
+        visited.set(start, null);
+        stack.push(startVertex);
+
+        // Evaluation
+        while (!stack.isEmpty()) {
+            GraphVertex<T, Integer> vertex = stack.pop();
+            if (visited.get(vertex.value) != null) continue;
+        
+            if (vertex.value == end) break;
+            visited.set(vertex.value, true);
+
+            for (GraphEdge<T, Integer> edge : vertex.edges) {
+                int newDistance = distance.get(vertex.value) + edge.weight;
+                Integer nextDistance = distance.get(edge.vertex.value);
+                if (nextDistance == null || newDistance < nextDistance) {
+                    distance.set(edge.vertex.value, newDistance);
+                    previous.set(edge.vertex.value, vertex.value);
+                    stack.push(edge.vertex);
+                }
+            }
+        }
+
+        // Result
+        ArrayList<T> path = new ArrayList<>();
+        T prev = previous.get(end);
+        if (prev != null) path.add(end);
+        while (prev != null) {
+            path.add(prev);
+            prev = previous.get(prev);
+        }
+        return path;
+    }
+
 }
--- src/test/java/com/pedraza/algorithms/GraphDijkstraTest.java
+++ src/test/java/com/pedraza/algorithms/GraphDijkstraTest.java
@@ -1,5 +1,66 @@
 package com.pedraza.algorithms;
 
+import static org.junit.Assert.*;
+
+import com.pedraza.datastructures.Graph;
+
+import org.junit.Test;
+
 public class GraphDijkstraTest {
-    
+
+    Graph<String, Integer> graph;
+    GraphDijkstra<String> search;
+
+    @Test
+    public void searchShouldReturnIndexWhenFound() {
+        graph = buildGraph();
+        search = new GraphDijkstra<>(graph);
+        Object[] actual = search.search("SFO", "JFK").toArray();
+        Object[] expected = new String[] { "JFK", "DCA", "ATL", "DFW", "LAS", "SFO" };
+        assertArrayEquals(expected, actual);
+    }
+
+    private Graph<String, Integer> buildGraph() {
+        Graph<String, Integer> graph = new Graph<>();
+
+        // Vertices
+        graph.addVertex("DFW"); // Dallas
+        graph.addVertex("LAS"); // Las Vegas 
+        graph.addVertex("LAX"); // Los Angeles
+        graph.addVertex("SFO"); // San Francisco 
+        graph.addVertex("ORD"); // Chicago
+        graph.addVertex("JFK"); // New York
+        graph.addVertex("DCA"); // Washington, DC
+        graph.addVertex("ATL"); // Atlanta
+        graph.addVertex("MIA"); // Miami
+
+        // Outbound
+        graph.addEdge("SFO", "LAS", 1);
+        graph.addEdge("SFO", "LAX", 2);
+        graph.addEdge("LAX", "LAS", 1);
+        graph.addEdge("LAS", "DFW", 4);
+        graph.addEdge("DFW", "ORD", 7);
+        graph.addEdge("DFW", "ATL", 3);
+        graph.addEdge("ORD", "JFK", 1);
+        graph.addEdge("JFK", "DCA", 1);
+        graph.addEdge("DCA", "JFK", 1);
+        graph.addEdge("ATL", "DCA", 2);
+        graph.addEdge("ATL", "MIA", 2);
+
+        // Inbound
+        graph.addEdge("LAS", "SFO", 1);
+        graph.addEdge("LAX", "SFO", 2);
+        graph.addEdge("LAS", "LAX", 1);
+        graph.addEdge("DFW", "LAS", 4);
+        graph.addEdge("ORD", "DFW", 7);
+        graph.addEdge("ATL", "DFW", 3);
+        graph.addEdge("JFK", "ORD", 1);
+        graph.addEdge("DCA", "JFK", 1);
+        graph.addEdge("JFK", "DCA", 1);
+        graph.addEdge("DCA", "ATL", 2);
+        graph.addEdge("MIA", "ATL", 2);
+
+        return graph;
+    }
+
 }
